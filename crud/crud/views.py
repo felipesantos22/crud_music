64a74403f8fcc08f2e9e65b15e18c5ec32aaa933
felipesantos22 <felipevs2018@gmail.com>
2024-09-11 03:05:46 -0300

    first commit

---
+++ crud/crud/views.py
@@ -0,0 +1,8 @@
+from rest_framework import generics
+from .models import Music
+from .serializers import MusicSerializer
+
+class MusicList(generics.ListCreateAPIView):
+
+    queryset = Music.objects.all()
+    serializer_class = MusicSerializer
